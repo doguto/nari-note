c3feed0aaf316653b298e7f9508d4c9386203997
doguto <doguto110@gmail.com>
2025-10-19 18:37:32 +0900

    Feat: HealthCheck用エンドポイントの作成

--- nari-note-backend/Src/Controller/HealthController.cs
+++ nari-note-backend/Src/Controller/HealthController.cs
@@ -1,21 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace NariNoteBackend.Src.Controller
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class HealthController : ControllerBase, IHealthCheck
+    public class HealthController : ControllerBase
     {
         [HttpGet]
         public IActionResult GetHealth()
         {
             return Ok(new { status = "Healthy" });
-        }
-
-        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
-        {
-            return Task.FromResult(HealthCheckResult.Healthy("The application is healthy."));
         }
     }
 }
