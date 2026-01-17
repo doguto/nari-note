de78c6254be431705fc3a6cdf829df7dc47e8342
kuto <doguto110@gmail.com>
2026-01-17 17:29:34 +0900

    Fix: Migrationの再生成

--- nari-note-backend/Migrations/NariNoteDbContextModelSnapshot.cs
+++ nari-note-backend/Migrations/NariNoteDbContextModelSnapshot.cs
@@ -3,7 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using NariNoteBackend.Infrastructure;
+using NariNoteBackend.Infrastructure.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
