d44404953d9822e8df964342fef7dab3576c08e2
kuto <doguto110@gmail.com>
2026-01-19 02:21:56 +0900

    Feat: メタデータの更新

--- nari-note-frontend/src/app/layout.tsx
+++ nari-note-frontend/src/app/layout.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import { QueryProvider } from "@/lib/providers/QueryProvider";
 import { AuthProvider } from "@/lib/providers/AuthProvider";
+
+export const metadata: Metadata = {
+  title: "将棋ブログ投稿サイト ～なりノート～",
+  description: "将棋の知識共有プラットフォーム\nあなたの将棋の知識を共有し、コミュニティと共に成長しましょう",
+};
 
 export default function RootLayout({
   children,
