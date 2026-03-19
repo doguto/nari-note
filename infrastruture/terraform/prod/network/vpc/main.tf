6043a66a09add4a3642814d96acf405512c6e182
kuto <doguto110@gmail.com>
2026-03-19 14:28:40 +0900

    Refactor: 調整

--- infrastruture/terraform/prod/network/vpc/main.tf
+++ infrastruture/terraform/prod/network/vpc/main.tf
@@ -1,7 +1,12 @@
+locals {
+  # 2つのAZを指定
+  availability_zone_count = length(var.availability_zones)
+}
+
 resource "aws_vpc" "nari-note-vpc" {
   # 最初の16bitをネットワーク部として固定する
-  # 10.0.0.0 ~ 10.0.255.255 の範囲を通信で使用可能
-  cidr_block = "10.0.0.0/16"
+  # RFC1918 のCに相当する範囲を通信で使用可能
+  cidr_block = "192.168.0.0/16"
 
   tags = {
     Name = "${var.app_name}-vpc"
@@ -10,7 +15,7 @@ resource "aws_vpc" "nari-note-vpc" {
 
 # コストの削減のため、AZは2つのみに分割する
 resource "aws_subnet" "nari-note-public-subnet" {
-  count = 2
+  count = local.availability_zone_count
 
   vpc_id                  = aws_vpc.nari-note-vpc.id
   map_public_ip_on_launch = true
@@ -18,7 +23,7 @@ resource "aws_subnet" "nari-note-public-subnet" {
 
   # 最初の24bitをネットワーク部として固定する
   # 10.0.i.0 ~ 10.0.i.255 の範囲をサブネット内の通信で使用可能
-  cidr_block = "10.0.${count.index}.0/24"
+  cidr_block = "192.168.${count.index}.0/24"
 
   tags = {
     Name = "${var.app_name}-public-subnet-${count.index}"
@@ -26,7 +31,7 @@ resource "aws_subnet" "nari-note-public-subnet" {
 }
 
 resource "aws_subnet" "nari-note-private-subnet" {
-  count = 2
+  count = local.availability_zone_count
 
   vpc_id                  = aws_vpc.nari-note-vpc.id
   map_public_ip_on_launch = false
@@ -35,7 +40,7 @@ resource "aws_subnet" "nari-note-private-subnet" {
   # 最初の24bitをネットワーク部として固定する
   # 10.0.(2 + i).0 ~ 10.0.(2 + i).255 の範囲をサブネット内の通信で使用可能
   # public-subnetと重複しないように、iに2を足す
-  cidr_block = "10.0.${2 + count.index}.0/24"
+  cidr_block = "192.168.${local.availability_zone_count + count.index}.0/24"
 
   tags = {
     Name = "${var.app_name}-private-subnet-${count.index}"
