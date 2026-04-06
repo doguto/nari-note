5e7ea4459a18d3f0334b8ea58a7db1c60e803ee2
kuto <doguto110@gmail.com>
2026-04-07 02:00:30 +0900

    Feat: nginxの起動

--- infrastruture/terraform/prod/app/server/userdata.sh
+++ infrastruture/terraform/prod/app/server/userdata.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+set -e
+
+# パッケージの更新
+dnf update -y
+
+# nginx
+dnf install -y nginx
+
+# nginx の自動起動を有効化&起動
+systemctl enable --now nginx
+
+
+# Application
+dnf install -y aspnetcore-runtime-9.0
