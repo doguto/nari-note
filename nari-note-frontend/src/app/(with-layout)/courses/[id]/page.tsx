2245cf36926e1230edd1f94a50100a957bd807dd
kuto <doguto110@gmail.com>
2026-04-13 01:05:36 +0900

    Fix: buildエラーの解消

--- nari-note-frontend/src/app/(with-layout)/courses/[id]/page.tsx
+++ nari-note-frontend/src/app/(with-layout)/courses/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import type { Metadata } from 'next';
 import { CourseDetailPage } from '@/features/course/pages';
 import { getCourseContent } from '@/lib/api/server';
--- nari-note-frontend/src/app/sitemap.ts
+++ nari-note-frontend/src/app/sitemap.ts
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import { MetadataRoute } from 'next';
 import { getArticles } from '@/lib/api/server';
 
