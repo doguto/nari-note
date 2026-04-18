c811780f7508a99fe9526b3ef0b64a0041eb035f
kuto <doguto110@gmail.com>
2026-04-19 01:21:37 +0900

    Feat: 記事系のレスポンシブデザイン対応

--- nari-note-frontend/src/features/global/organisms/PageWithoutSidebar.tsx
+++ nari-note-frontend/src/features/global/organisms/PageWithoutSidebar.tsx
@@ -7,7 +7,7 @@ interface PageWithoutSidebarProps {
 
 export function PageWithoutSidebar({ children, title }: PageWithoutSidebarProps) {
   return (
-    <div className="sm:w-full md:min-w-11/12 w-11/12 mx-auto px-4 py-8 flex justify-center">
+    <div className="sm:w-full md:min-w-11/12 w-11/12 mx-auto px-2 py-6 flex justify-center">
       <main className="flex-1 max-w-5xl">
         {title && <h2 className="text-2xl font-bold text-brand-text mb-6">{title}</h2>}
         {children}
